d9050a8951b92c91333c89dbebcc489fe6f22beb
Martim Barros <mpdb2016@gmail.com>
2025-11-11 11:34:32 +0000

    Refactor move method in GameCharacter to use directional input instead of random positioning

--- src/objects/GameCharacter.java
+++ src/objects/GameCharacter.java
@@ -1,6 +1,5 @@
 package objects;
 
-import java.util.Random;
 import pt.iscte.poo.game.Room;
 import pt.iscte.poo.utils.Point2D;
 import pt.iscte.poo.utils.Vector2D;
@@ -13,9 +12,8 @@ public abstract class GameCharacter extends GameObject {
 	}
 	
 	public void move(Vector2D dir) {
-		Random rand = new Random();
-		Point2D destination = new Point2D(rand.nextInt(10), rand.nextInt(10)); 
-		setPosition(destination);		
+		Point2D startPosition = getPosition();
+		setPosition(startPosition.plus(dir));		
 	}
 
 	@Override

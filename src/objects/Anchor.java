ad4733c3d966b1679ae17bb626469af3fc5c3ee7
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-11-12 08:32:42 +0000

    Refactor canMove method formatting in Anchor.java
    Removed unnecessary blank lines and improved readability of the instanceof condition in the canMove method. No functional changes were made.

--- src/objects/Anchor.java
+++ src/objects/Anchor.java
@@ -29,23 +29,21 @@ public class Anchor extends MovableObject {
 
     @Override
     public boolean canMove(Point2D from, Point2D to, Vector2D dir, GameObject cla) {
-
         if (MovedOnce) {
             return false;
         }
-
         if (!Point2D.sameDirectionHorzontal(from, to)) {
             return false;
         }
-
         for (GameObject obj1 : cla.getRoom().getObjects()) {
             if (obj1.getPosition().equals(to)) {
-                if (obj1 instanceof NonMovable || obj1 instanceof MovableObject || obj1 instanceof GameCharacter) {
+                if (obj1 instanceof NonMovable 
+				|| obj1 instanceof MovableObject
+				|| obj1 instanceof GameCharacter) {
                     return false;
                 }
             }
         }
-
         MovedOnce = true;
         return true;
     }

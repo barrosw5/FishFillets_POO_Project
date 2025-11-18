3f622f04884cc09e6a0c18c523868f736ab78375
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-11-18 11:21:29 +0000

    Remove unused Point2D field from MovableObject
    Eliminated the unused 'startingPosition' field and its import from MovableObject.java to clean up the code.

--- src/objects/MovableObject.java
+++ src/objects/MovableObject.java
@@ -1,11 +1,9 @@
 package objects;
 
 import pt.iscte.poo.game.Room;
-import pt.iscte.poo.utils.Point2D;
 
 public abstract class MovableObject extends GameObject{
     private static boolean isMovable = true;
-    private Point2D startingPosition;
     
     public MovableObject(Room room) {
         super(room, isMovable);

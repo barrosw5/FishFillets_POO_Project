87ad86e2a623f3c5982833a0c91a8b847eb7d92f
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-11-25 07:51:18 +0000

    Update Bigfish with new logical ( building..)

--- src/objects/BigFish.java
+++ src/objects/BigFish.java
@@ -49,52 +49,72 @@ public class BigFish extends GameCharacter {
 	// Na vertical só consegue empurrar um 
 	// O código ainda, creio eu, pode ser mais descentralizado, ou seja, criar mini funções, farei isso depois. Para já está funcional. 
 	// A gravidade está em construção...
-	@Override
+	// @Override
+	// public boolean fishCanMove(Point2D pos, Vector2D dir) {
+	// 	if (hasWon())
+	// 		return false;
+
+	// 	Point2D finalPos = pos.plus(dir);
+		
+	// 	for ( GameObject obj: this.getRoom().getObjects() ) {
+	// 		if ( obj.getPosition().equals(finalPos)) { // Verifica se existe algum objeto na sua frente 
+	// 			if (Point2D.sameDirectionHorzontal(pos, finalPos)) { // Se sim, vê se a direção que o user que seguir é na horizontal com a função q criei e está no Point2D
+	// 				Point2D firstObjTarget = finalPos.plus(dir); // Primeiro objeto 
+	// 				Point2D secondObjTarget = firstObjTarget.plus(dir); // Segundo objeto 
+
+	// 				if (obj instanceof MovableObject) { // Se o primeiro for móvel vai verificar o segundo e tal 
+	// 					for (GameObject obj2 : this.getRoom().getObjects()) {
+	// 						if (obj2.getPosition().equals(firstObjTarget)) {
+	// 							if (obj2 instanceof MovableObject &&
+	// 								(obj2.canMoveLightObject(firstObjTarget, secondObjTarget, dir, this) ||
+	// 								obj2.canMoveHeavyObject(firstObjTarget, secondObjTarget, dir, this))) {
+	// 								pushTwoObject(obj, obj2, pos, firstObjTarget, secondObjTarget); // Ele usa a função pushTwoObject que criei e que está na classe GameCharacter
+	// 								return true;
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+				
+	// 			if ( obj instanceof MovableObject && (obj.canMoveHeavyObject(finalPos, finalPos.plus(dir), dir, this) || obj.canMoveLightObject(finalPos, finalPos.plus(dir), dir, this))) {
+	// 					Point2D FinalObPos = finalPos.plus(dir); // Se for só um objeto ele faz um processo semelhante, mas mais simples 
+	// 					pushObject(obj, finalPos, FinalObPos);				
+	// 				return true;
+				
+	// 			}
+
+	// 			if ( obj instanceof KillBigFish  || obj instanceof KillAllFishes) { // adicionado a função que mata o peixe grande quando este toca na Trap 
+	// 				dead(bf, this);
+	// 			}
+
+	// 			if ( (obj instanceof MovableObject || obj instanceof NonMovableObject || obj instanceof GameCharacter ) ) {
+	// 				return false; // Restrições habituais do movimento do peixe grande 
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return true;
+	
+	// }
+
 	public boolean fishCanMove(Point2D pos, Vector2D dir) {
 		if (hasWon())
 			return false;
 
 		Point2D finalPos = pos.plus(dir);
-		
-		for ( GameObject obj: this.getRoom().getObjects() ) {
-			if ( obj.getPosition().equals(finalPos)) { // Verifica se existe algum objeto na sua frente 
-				if (Point2D.sameDirectionHorzontal(pos, finalPos)) { // Se sim, vê se a direção que o user que seguir é na horizontal com a função q criei e está no Point2D
-					Point2D firstObjTarget = finalPos.plus(dir); // Primeiro objeto 
-					Point2D secondObjTarget = firstObjTarget.plus(dir); // Segundo objeto 
+		GameObject obj = findObject(finalPos, getRoom());
+		Point2D finalObjPos = finalPos.plus(dir);
 
-					if (obj instanceof MovableObject) { // Se o primeiro for móvel vai verificar o segundo e tal 
-						for (GameObject obj2 : this.getRoom().getObjects()) {
-							if (obj2.getPosition().equals(firstObjTarget)) {
-								if (obj2 instanceof MovableObject &&
-									(obj2.canMoveLightObject(firstObjTarget, secondObjTarget, dir, this) ||
-									obj2.canMoveHeavyObject(firstObjTarget, secondObjTarget, dir, this))) {
-									pushTwoObject(obj, obj2, pos, firstObjTarget, secondObjTarget); // Ele usa a função pushTwoObject que criei e que está na classe GameCharacter
-									return true;
-								}
-							}
-						}
-					}
-				}
-				
-				if ( obj instanceof MovableObject && (obj.canMoveHeavyObject(finalPos, finalPos.plus(dir), dir, this) || obj.canMoveLightObject(finalPos, finalPos.plus(dir), dir, this))) {
-						Point2D FinalObPos = finalPos.plus(dir); // Se for só um objeto ele faz um processo semelhante, mas mais simples 
-						pushObject(obj, finalPos, FinalObPos);				
-					return true;
-				
-				}
-
-				if ( obj instanceof KillBigFish  || obj instanceof KillAllFishes) { // adicionado a função que mata o peixe grande quando este toca na Trap 
-					dead(bf, this);
-				}
-
-				if ( (obj instanceof MovableObject || obj instanceof NonMovableObject || obj instanceof GameCharacter ) ) {
-					return false; // Restrições habituais do movimento do peixe grande 
-				}
-			}
+		if ( obj == null) {
+			return true;
 		}
 
-		return true;
-	
+		if ( obj instanceof Interactable) {
+			return ((Interactable) obj).interagirPeixe(this, finalPos, finalObjPos, dir);
+		}
+
+		return false;
+
 	}
 
 	@Override

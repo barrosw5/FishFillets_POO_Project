fbfba001eaa134d997907fd7a35aa010ac88ebd3
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-11-25 11:30:41 +0000

    Update Juan

--- src/objects/Juan.java
+++ src/objects/Juan.java
@@ -1,8 +1,10 @@
 package objects;
 
 import pt.iscte.poo.game.Room;
+import pt.iscte.poo.utils.Point2D;
+import pt.iscte.poo.utils.Vector2D;
 
-public class Juan extends LightObject{
+public class Juan extends LightObject implements Interactable{
 
     public Juan(Room room){
         super(room);
@@ -23,6 +25,28 @@ public class Juan extends LightObject{
         
     }
 
+    @Override
+    public boolean interactWithFish(GameCharacter fish, Point2D from, Point2D to, Vector2D dir) {
+        	if ( JuanPushBy(fish, from, to, dir)) {
+			pushObject(this, from, to);
+			return true;
+		}
+		return false;
+    }
+
+    public boolean JuanPushBy(GameCharacter fish, Point2D from, Point2D to, Vector2D dir) {
+		GameObject obj = findObject(to, fish.getRoom());
+
+		if ( fish instanceof BigFish ) {
+			this.getRoom().removeObject(fish);
+            return false;
+		}
+
+		if ( (obj instanceof MovableObject || obj instanceof NonMovableObject || obj instanceof GameCharacter) && !(obj instanceof HoledWall)) {
+			return false;
+		}
+		return true;
+	}
     //testar polimorfism a partir daqui
     
 }

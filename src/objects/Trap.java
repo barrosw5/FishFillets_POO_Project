7f8fd2403fb6ff972a4c86312884ad1156788b20
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-11-25 07:55:16 +0000

    Update Trap to BigFish

--- src/objects/Trap.java
+++ src/objects/Trap.java
@@ -25,7 +25,7 @@ public class Trap extends NonMovableObject implements Interactable {
 		if ( fish instanceof SmallFish) {
 			return true;
 		}
-		removeObject(fish, fish.getRoom());
+		fish.getRoom().removeObject(fish);
 		return false;
 	}	
 

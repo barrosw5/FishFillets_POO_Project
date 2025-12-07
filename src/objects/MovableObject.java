657b28911440d1db4b56246538d163bf152d1c9c
Martim Barros <mpdb2016@gmail.com>
2025-12-07 20:10:48 +0000

    movable e nao movable

--- src/objects/MovableObject.java
+++ src/objects/MovableObject.java
@@ -3,15 +3,8 @@ package objects;
 import pt.iscte.poo.game.Room;
 
 public abstract class MovableObject extends GameObject {
-    private static final boolean isMovable = true;
     
     public MovableObject(Room room) {
-        super(room, isMovable);
+        super(room, true);
     }
-
-    @Override
-    public void reset() {
-        super.reset();
-    }
-
 }

ba66d2e56fe9651035e5b86f5bef8f60c92cd1f3
Pedro Coelho <pmnco1@iscte-iul.pt>
2025-12-07 20:17:20 +0000

    Coin done, finalmente

--- src/objects/Coin.java
+++ src/objects/Coin.java
@@ -23,21 +23,20 @@ public class Coin extends NonMovableObject implements Interactable {
     // se a moeda puder ser removida do jogo (sempre, praticamente), ele irá reduzir o tempo do player em 10s sempre q o fish comer uma. 
     @Override
     public boolean interactWithFish(GameCharacter fish, Point2D from, Point2D to, Vector2D dir) {
-        if ( CoinPushBy(fish, from, to, dir)) {
+        Point2D before = from.minus(dir);
+        GameObject obj = findObject(before, getRoom());
+        if ( obj instanceof GameCharacter ) {
+            GameObject.removeObject(this, getRoom());
             getRoom().reduceRealTime(10);
             pushObject(fish, from, to);
             return true;  
         }
+        
+        if (!(obj instanceof GameCharacter)) {
+            pushObject(fish, from, to);
+            return true; 
+        }
         return false;
     }
 
-    // Basicamente, se for qql um dos fishes a tocar na coin, a moeda é removida do jogo 
-    
-    public boolean CoinPushBy(GameCharacter fish, Point2D from, Point2D to, Vector2D dir) {
-        if ( fish instanceof BigFish || fish instanceof SmallFish) {
-            GameObject.removeObject(this, getRoom());
-            return true;
-        }
-       return false;
-    }
 }
